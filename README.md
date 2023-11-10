7cf4312646901d4bf569b5fca8bc64ea5d731e3e
youhaowen <youhaowen@bytedance.com>
2023-11-10 16:58:19 +0800

    update

--- README.md
+++ README.md
@@ -3,16 +3,56 @@
 ## 功能
 
 - [X] 修改每个文档的第一段话后+第x期（会破坏原有的样式）
-- [X] 合并每个word文档的第一段话和第二段话
 - [X] 输出每个word文档第一段话和第二段话的在/output.txt
 - [ ] 把正文的第一句话改为标题级别
-- [ ] 合并所有word到一个words
 
 ## 用法
 
 1. 在当前环境下安装 `python-docx`库：pip install python-docx
-2. 在根目录下运行：python check_docx_title.py
+2. 在根目录下运行：
+   1. check_docx_title.py 修改第一段话名称
+   2. merge.py 合并docx到同一个文件
 
-## 参考
+# merge
 
-https://python-docx.readthedocs.io/en/latest/
+实现自动合并word的python脚本文件
+
+- 保留所有文档固有的样式
+- 保留文档的顺序
+
+## 使用方法
+
+**【注意】** 只能合并 `.docx`文件，若需要合并的文档中存在 `.doc`文件，需先手动将其转换为 `.docx`文件才能使用此脚本
+
+- 在该项目的根目录下创建 `files`文件夹，将需要合并的 `.docx`文件放进去
+- 运行 `merge.py`，即可得到合并结果文件 `merge_result.docx`
+
+## 特性说明
+
+### 文档分页
+
+每个文档自动从新一页开始，两个文档之间会插入一个分页符
+
+### 文档合并顺序
+
+文档的合并顺序与在资源管理器中的排序一致，因此对排序有强要求的需求，最好对文档进行编号
+
+# 鸣谢
+
+参考引用：https://github.com/FutureXZC/auto-merge-docx（代码merge.py）
+
+常见文档批量操作：https://zhuanlan.zhihu.com/p/323680114
+
+mac 安装brew：https://www.cnblogs.com/liyihua/p/12753163.html
+
+doc转docx：
+
+https://zhuanlan.zhihu.com/p/649925993
+
+https://www.cnblogs.com/bubblebeee/p/17096397.html
+
+https://blog.csdn.net/S1mpleboy6/article/details/132190913
+
+https://zhuanlan.zhihu.com/p/561923128
+
+https://www.wenjiangs.com/article/r05nnmmsj10d.html
--- check_docx_title.py
+++ check_docx_title.py
@@ -1,12 +1,16 @@
 import os  
 from docx import Document
-
+from tqdm import tqdm 
+# 参考
 # https://python-docx.readthedocs.io/en/latest/
 # pip install python-docx
+# https://zhuanlan.zhihu.com/p/377134370
+# http://www.docpe.com/word/combine-word.aspx
 
 
 output_folder = "data/output"
-
+input = "data/05 产业上中下游价格指数周度报告/产业上中下游价格指数报告_49篇"
+# input = "data/06 高频产销存数据分析快报/高频产销存数据分析快报_49篇"
 if not os.path.exists(output_folder):  
     os.makedirs(output_folder)  
     print(f"Path {output_folder} created.")  
@@ -16,26 +20,25 @@ else:
 document=Document()
 
 # 获取文件列表并排序  
-file_list = os.listdir("data/05 产业上中下游价格指数周度报告/产业上中下游价格指数报告_49篇")  
+file_list = os.listdir(input)  
 file_list.sort()
-print(file_list)
+# print(file_list)
 # 创建一个新文件，如果文件已经存在则会被覆盖  
 
 with open("output.txt", "w") as file:  
     file.write(f"输出结果：\n")  
-    for i, filename in enumerate(file_list):  
+    for i, filename in enumerate(file_list):
         if filename.endswith(".docx"):  
-            doc_path = os.path.join("data/05 产业上中下游价格指数周度报告/产业上中下游价格指数报告_49篇", filename)  
+            doc_path = os.path.join(input, filename)  
             doc = Document(doc_path)  
             first_paragraph = doc.paragraphs[0].text  
             second_paragraph = doc.paragraphs[1].text  
-            first_paragraph = f'{first_paragraph} 第（{i+1}）期'  
+            first_paragraph = f'{first_paragraph}（第{i+1}期）'  
             print(filename, first_paragraph, second_paragraph)  
-            doc.paragraphs[0].text = first_paragraph  
-            
+            doc.paragraphs[0].text = first_paragraph 
             output_filename = os.path.join(output_folder, filename)  
             doc.save(output_filename)  
-            with open('output.txt', 'a+') as f:  
+            with open('output_title.txt', 'a+') as f:  
                 f.write(f"{filename}: {first_paragraph}")  
                 f.write(f"{second_paragraph}\n")
-            print(f"文档已修改并保存到：{output_filename}")
+            # print(f"文档已修改并保存到：{output_filename}")
